a6cf16f2b328cab7250ccf8a828a11de027d4311
GraziellaPereira <graziellapereira52@gmail.com>
2025-04-03 22:52:26 -0300

    terminado sentinela

--- BuscaLinear-PrimeiroPar/Program.cs
+++ BuscaLinear-PrimeiroPar/Program.cs
@@ -4,29 +4,29 @@ namespace BuscaLinear_PrimeiroPar
 {
     class Program
     {
-        static bool BuscaPrimeiroPar(int[] array, int elemento, out int multiplo)
+        static bool BuscaPrimeiroPar(int[] array, out int primeiroPar)
         {
             foreach (int num in array)
             {
-                if (num % elemento == 0)
+                if (num % 2 == 0)
                 {
-                    multiplo = num;
+                    primeiroPar = num;
                     return true;
                 }
             }
-            multiplo = -1;
+            primeiroPar = -1;
             return false;
 
         }
 
         static void Main()
         {
-            int[] numeros = { 10, 25, 30, 40, 55, 60 };
+            int[] numeros = { 17, 10, 25, 30, 40, 55, 60 };
             int primeiroPar;
-            if (BuscaLinear(numeros, chave, out multiplo))
-                Console.WriteLine($"Primeiro número múltiplo de {chave} encontrado = {multiplo}.");
+            if (BuscaPrimeiroPar(numeros, out primeiroPar))
+                Console.WriteLine($"Primeiro número par encontrado: {primeiroPar}");
             else
-                Console.WriteLine($"Nenhum múltiplo de {chave} encontrado.");
+                Console.WriteLine($"Nenhum número par encontrado.");
 
         }
     }
---
+++ BuscaLinear-Sentinela/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BuscaLinear_entinela
+{
+    class Program
+    {
+        static int BuscaLinearSentinela(int[] array, int elemento)
+        {
+            int n = array.Length;
+            int ultimo = array[n - 1]; 
+            array[n - 1] = elemento;  
+
+            int i = 0;
+            while (array[i] != elemento)
+            {
+                i++;
+            }
+
+            array[n - 1] = ultimo; 
+
+            
+            if (i < n - 1 || (array[n - 1] == elemento && ultimo == elemento))
+                return i;
+
+            return -1;
+        
+    }
+    
+
+        static void Main()
+        {
+            int[] numeros = { 10, 25, 30, 40, 55, 60 };
+            int chave = 40;
+            int resultado = BuscaLinearSentinela(numeros, chave);
+            Console.WriteLine(resultado != -1 ? $"Elemento encontrado na posição {resultado}" : "Elemento não encontrado");
+            Console.ReadKey();
+        }
+    }
+
+}
+
